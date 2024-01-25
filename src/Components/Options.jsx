d30caee8928b17b2c767722091f21bdfa5696554
zubbey <com.zubbey@hotmail.com>
2024-01-25 18:40:56 +0100

    Made small changes in theme options

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 export const Options = () => {
-  let { lsTheme, lsIcon } = "";
+  let { lsTheme, lsIcon } = '';
   try {
-    lsTheme = localStorage.getItem("theme");
-    lsIcon = localStorage.getItem("icon");
+    lsTheme = localStorage.getItem('theme');
+    lsIcon = localStorage.getItem('icon');
   } catch (e) {
     console.error(`All Cookies blocked - Error: ${e.message}`);
   }
 
-  const [theme, setTheme] = useState(lsTheme || "light");
-  const [icon, setIcon] = useState(lsIcon || "bx-moon");
+  const [theme, setTheme] = useState(lsTheme || 'dark');
+  const [icon, setIcon] = useState(lsIcon || 'bx-moon');
 
   useEffect(() => {
-    localStorage.setItem("theme", theme);
-    localStorage.setItem("icon", icon);
-    document.body.classList[theme === "dark" ? "add" : "remove"]("dark-theme");
+    localStorage.setItem('theme', theme);
+    localStorage.setItem('icon', icon);
+    document.body.classList[theme === 'dark' ? 'add' : 'remove']('dark-theme');
   }, [theme, icon]);
 
   const _toggleTheme = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-    icon === "bx-sun" ? setIcon("bx-moon") : setIcon("bx-sun");
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+    icon === 'bx-sun' ? setIcon('bx-moon') : setIcon('bx-sun');
   };
 
   return (
-    <div className="home__options">
+    <div className='home__options'>
       <i
         className={`bx ${icon} change-theme`}
-        title="Toggle Theme"
-        id="theme-button"
+        title='Toggle Theme'
+        id='theme-button'
         onClick={_toggleTheme}
       />
     </div>
